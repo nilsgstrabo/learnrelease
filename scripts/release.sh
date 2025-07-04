73c6ec06dc2393b2456aa19c996d68905639801f
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-07-04 16:34:28 +0200

    ci: test

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-json=$(gh pr list --json title,labels,mergeCommit,state,number --state merged --label "release: pending")
+# json=$(gh pr list --json title,labels,mergeCommit,state,number --state merged --label "release: pending")
 
-jq -c '.[]' <(echo "$json") | while read i; do
-    commit=$(jq .mergeCommit.oid -r <(echo "$i"))
-    echo $i
-    git show "$commit":charts/learnrelease/Chart.yaml | yq .version
-done
+# jq -c '.[]' <(echo "$json") | while read i; do
+#     commit=$(jq .mergeCommit.oid -r <(echo "$i"))
+#     echo $i
+#     git show "$commit":charts/learnrelease/Chart.yaml | yq .version
+# done
+
+pr_info=$(gh pr view $1 --json title,labels,mergeCommit,state,number | jq 'select(.state=="MERGED" and (.labels[] | .name=="release: pending"))')
+
+[[ -z "$pr_info" ]] && { echo "Pull request ${1} does not exist or has invalid state"; exit 1; }
+
+echo $pr_info
