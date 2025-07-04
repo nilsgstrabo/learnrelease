80d244d1187c176ff3ea5954f5080b2b124f6f2c
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-07-04 15:34:03 +0200

    ci: test

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-json=$(gh pr list --json title,labels,mergeCommit,state,id --state merged --label "release: pending")
+json=$(gh pr list --json title,labels,mergeCommit,state,number --state merged --label "release: pending")
 
 jq -c '.[]' <(echo "$json") | while read i; do
     commit=$(jq .mergeCommit.oid -r <(echo "$i"))
