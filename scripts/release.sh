faab192f8c2faee7a9f456adb43ad54be98d11fa
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-07-08 10:37:57 +0200

    ci: detect if version is prerelease

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,6 +11,7 @@
 
 set -Eeuo pipefail
 
+
 pr_number=$1
 
 pr_info=$(gh pr view $pr_number --json title,labels,mergeCommit,state,number,body | jq 'select(.state=="MERGED" and (.labels[] | .name=="release: pending"))')
@@ -21,6 +22,12 @@ version=$(git show "$commit":charts/learnrelease/Chart.yaml | yq .version)
 version=${version#v}
 tag=v${version}
 release_body=$(echo $pr_info | jq -r .body)
+is_prerelease="false"
+if [[ "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+- ]]; then
+    is_prerelease="true"
+fi
+
+
 echo $pr_info | jq .body
 
 echo $commit
@@ -31,7 +38,6 @@ echo "$release_body"
 
 
 
-
-gh release create $tag --notes "$release_body" --target $commit --title "$tag"
+gh release create $tag --notes "$release_body" --target $commit --title "$tag" --prerelease=$is_prerelease
 # gh pr edit $1 --remove-label "release: pending" --add-label "release: tagged"
 # Docker build + push (hvilke tags)
