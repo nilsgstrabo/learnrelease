24e4125018139e43a675b75b14315b515c1a421e
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-07-08 10:54:07 +0200

    fix: root 2

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func initHandler() http.Handler {
 
 func rootHandler(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
-	w.Write([]byte("root #1"))
+	w.Write([]byte("root #2"))
 }
 
 func apiHandler(w http.ResponseWriter, req *http.Request) {
